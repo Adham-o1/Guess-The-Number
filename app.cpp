83c7b91753e1372a7d379ecb4e93d7315fdecdfc
Adham Hossam <30608221402793@sci.asu.edu.eg>
2025-11-18 15:49:04 +0200

    Initial commit

--- app.cpp
+++ app.cpp
@@ -3,55 +3,26 @@ using namespace std;
 
 int main ()
 {
-    int points = 0;
-    int answers [3] = {};
-    int seq[3][5] = {
-        {1, 5, 10, 16, 23},
-        {2, 4, 8, 16, 32},
-        {1, 1, 2, 3, 5}};
-
-        cout << "====================================\n";
-        cout << "Type The Missing Number In Sequences\n";
-        cout << "====================================\n";
-
-        cout << "Q1:\n";
-        cout << "1 | 5 | 10 | 16 | ?? \n";
-        cout << "=> ";
-        cin >> answers[0];
-
-        cout << "=====================\n";
-        cout << "Q2:\n";
-        cout << "2 | 4 | 8 | 16 | ?? \n";
-        cout << "=> ";
-        cin >> answers[1];
-
-        cout << "=====================\n";
-        cout << "Q3:\n";
-        cout << "1 | 1 | 2 | 3 | ?? \n";
-        cout << "=> ";
-        cin >> answers[2];
-
-        cout << "===========================\n";
-        
-        if (answers[0] == seq[0][4])
+    int num = 0, sum = 0, count = 0;
+    double mean = 0;
+    cout << "Enter Positive Numbers Only: ";
+    cin >> num;
+    if (num > 0)
+    {
+        sum += num;
+        count++;
+        while (num > 0)
         {
-            points++ ;
-        }else {
-            cout << "[1] The correct answer : " << seq[0][4] << endl;
+            cout << "Enter Positive Numbers Only: ";
+            cin >> num;
+            if (num > 0)
+            {
+                sum += num;
+                count++;
+            }
         }
-        if (answers[1] == seq[1][4])
-        {
-            points++ ;
-        }else {
-            cout << "[2] The correct answer : " << seq[1][4] << endl;
-        }
-        if (answers[2] == seq[2][4])
-        {
-            points++ ;
-        }else {
-            cout << "[3] The correct answer : " << seq[2][4] << endl;
-        }
-        
-        cout << "Your Points : "<< points << " / 3" << endl;
+        mean = float(sum) / float(count);
+        cout << "The Arithmetic Mean: " << mean << endl;
+    }
     return 0;
 }
